76f49588d29090963aaf4cc4a928889f45f6078f
linlin <1208215066@qq.com>
2022-10-24 20:54:05 +0800

    使用了gin进行端口的设置和使用AsciiJSON 生成带有转义的非 ASCII 字符的纯 ASCII JSON

--- example.go
+++ example.go
@@ -1,17 +1,38 @@
 package main
-
 import (
-	"net/http"
-
-	"github.com/gin-gonic/gin"
+  "net/http"
+  "github.com/gin-gonic/gin"
 )
 
+//func main() {
+//  //r := gin.Default()
+//  //r.GET("/ping", func(c *gin.Context) {
+//  //  c.JSON(http.StatusOK, gin.H{
+//  //    "message": "pong",
+//  //  })
+//  //})
+//  router := gin.Default()
+//  router.GET("/hi", func(context *gin.Context) {
+//    context.String(http.StatusOK, "Hello world!")
+//  })
+//  err := router.Run()
+//  if err != nil {
+//    panic("[Error] failed to start Gin server due to: " + err.Error())
+//    return
+//  }
+//  router.Run(":9888")
+//  //r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+
+//}
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+    router := gin.Default()
+    router.GET("/hi", func(context *gin.Context) {
+        context.String(http.StatusOK, "Hello world!")
+    })
+    err := router.Run(":9888")
+    if err != nil {
+        panic("[Error] failed to start Gin server due to: " + err.Error())
+        return
+    }
 }
+
