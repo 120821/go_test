3596ef9de51e1d739df31b466c91a3d4d5292fc6
linlin <1208215066@qq.com>
2022-10-25 09:16:33 +0800

    把表单数据请求与自定义结构绑定

--- example.go
+++ example.go
@@ -3,6 +3,53 @@ import (
   "net/http"
   "github.com/gin-gonic/gin"
 )
+type StructA struct {
+    FieldA string `form:"field_a"`
+}
+
+type StructB struct {
+    NestedStruct StructA
+    FieldB string `form:"field_b"`
+}
+
+type StructC struct {
+    NestedStructPointer *StructA
+    FieldC string `form:"field_c"`
+}
+
+type StructD struct {
+    NestedAnonyStruct struct {
+        FieldX string `form:"field_x"`
+    }
+    FieldD string `form:"field_d"`
+}
+
+func GetDataB(c *gin.Context) {
+    var b StructB
+    c.Bind(&b)
+    c.JSON(200, gin.H{
+        "a": b.NestedStruct,
+        "b": b.FieldB,
+    })
+}
+
+func GetDataC(c *gin.Context) {
+    var b StructC
+    c.Bind(&b)
+    c.JSON(200, gin.H{
+        "a": b.NestedStructPointer,
+        "c": b.FieldC,
+    })
+}
+
+func GetDataD(c *gin.Context) {
+    var b StructD
+    c.Bind(&b)
+    c.JSON(200, gin.H{
+        "x": b.NestedAnonyStruct,
+        "d": b.FieldD,
+    })
+}
 
 //func main() {
 //  //r := gin.Default()
@@ -29,6 +76,9 @@ func main() {
     router.GET("/hi", func(context *gin.Context) {
         context.String(http.StatusOK, "Hello world!")
     })
+    router.GET("/getb", GetDataB)
+    router.GET("/getc", GetDataC)
+    router.GET("/getd", GetDataD)
     err := router.Run(":9888")
     if err != nil {
         panic("[Error] failed to start Gin server due to: " + err.Error())
