73eaccccf2e3502357435adfc09dce688d1b158e
linlin <1208215066@qq.com>
2022-10-26 16:53:00 +0800

    修改了gins.go的端口

--- gins.go
+++ gins.go
@@ -12,5 +12,6 @@ func main() {
       "message": "pong",
     })
   })
-  r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+//  r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+  r.Run(":8888")
 }
