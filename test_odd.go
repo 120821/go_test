f5dff2dc09a04c1d71d3ac2b0cff4a4d0ee79024
linlin <1208215066@qq.com>
2023-03-17 09:40:12 +0800

    测试了判断偶数

--- test_odd.go
+++ test_odd.go
@@ -1,15 +1,23 @@
 package main
 import "fmt"
-func print_odd_number (number int) int{
+import "strconv"
+//import "reflect"
+func print_odd_number (number int) []string{
   var n int
+  //var t []string
+  t := []string{}
   for n=1; n<=number; n++{
     if n%2==0{
       //fmt.Println("结果没有余数:", n)
+      t = append(t, strconv.Itoa(n))
     }else{
-      fmt.Println(n)
+      //fmt.Println(n)
+      //fmt.Println(n, reflect.TypeOf(n))
+      //t = append(t, strconv.Itoa(n))
+      //fmt.Println(t, reflect.TypeOf(t))
     }
   }
-  return n
+  return t
 }
 func main() {
   fmt.Println(print_odd_number(10))
