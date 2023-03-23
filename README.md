1a78a824f3dfc2b95575236561a9dd81ff42fc74
linlin <1208215066@qq.com>
2023-03-23 10:28:35 +0800

    二维数组

--- README.md
+++ README.md
@@ -436,3 +436,29 @@ get result:
 ```
 [[1] [2 2] [3 3 3] [4 4 4 4] [5 5 5 5 5]]
 ```
+
+### array2.go
+```
+package main
+
+import "fmt"
+
+func main() {
+  // 创建二维数组
+  sites := [2][2]string{}
+
+  // 向二维数组添加元素
+  sites[0][0] = "Google"
+  sites[0][1] = "Runoob"
+  sites[1][0] = "Taobao"
+  sites[1][1] = "Weibo"
+
+  // 显示结果
+  fmt.Println(sites)
+}
+```
+$ go run array2.go
+output:
+```
+[[Google Runoob] [Taobao Weibo]]
+```
--- generate_array.go
+++ generate_array.go
@@ -15,13 +15,13 @@ func generate_small_array(n int) []int{
 }
 
 func main() {
-  fmt.Println(generate_small_array(5))
+  //fmt.Println(generate_small_array(5))
   // 第二次循环
   result := [][]int{}
   for i := 1; i <= 5; i++{
-    fmt.Println(generate_small_array(5))
+    //fmt.Println(generate_small_array(5))
     result = append(result, generate_small_array(i))
-    fmt.Println("====== 每个循环2, i:", i, "result", result)
+    fmt.Println("======================= 每个循环2, i:", i, "result", result)
   }
 }
 
