1290007fa1f00692628b203c35dae01ccf48bf46
linlin <1208215066@qq.com>
2023-03-28 14:39:26 +0800

    返回质数以及质数的和

--- test_zhishu.go
+++ test_zhishu.go
@@ -1,13 +1,17 @@
 package main
 import "fmt"
-func zhiShu(number int) []int{
+func zhiShu(number int) ([]int, int){
   array := []int{}
+  sum := 0
   if number == 1 {
     array = append(array, 1)
+    sum = 1
   }else if number == 2{
     array = append(array, 1, 2)
+    sum = 1 + 2
   }else if number >= 2{
     array = append(array, 1, 2)
+    sum = 1 + 2
     for i := 2; i<=number; i++{
       for j := 2; j<i; j++{
         if i%j == 0 {
@@ -16,15 +20,16 @@ func zhiShu(number int) []int{
           if j+1 == i {
             //fmt.Println("i 是", i, "array", array)
             array = append(array, i)
+            sum = sum + i
           }
         }
       }
     }
   }
-  return array
+  return array, sum
 }
 
 func main() {
-  sort_array := zhiShu(3)
-  fmt.Println(sort_array)
+  sort_array, sum := zhiShu(30)
+  fmt.Println(sort_array, sum)
 }
